5963be92f455f455f248b437b497259accb5ebb7
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2022-06-13 17:31:08 +0400

    feat: implement email icon

--- src/packages/components/icons/index.ts
+++ src/packages/components/icons/index.ts
@@ -1,3 +1,4 @@
+export { default as EmailIcon } from "./Email";
 export { default as MoonIcon } from "./Moon";
 export { default as SunIcon } from "./Sun";
 export { default as TyIcon } from "./Ty";
