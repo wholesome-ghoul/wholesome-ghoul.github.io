0a65475282a23f8265ed9b9a8af5796d3acb1994
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2022-08-14 20:35:37 +0400

    chore: copy public files

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,6 +4,8 @@ import { generateWebpackConfig } from "@tubeu/configs-webpack"
 const config = {
   ...generateWebpackConfig({
     outputDir: path.resolve(__dirname, "dist"),
+    template: "./public/index.html",
+    publicUrl: process.env.PUBLIC_URL,
   }),
 }
 
